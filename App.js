19ca09312ddefd7226a49209a4c89e333d2e679a
Dave <dev@itsdave.de>
2023-07-31 16:10:52 +0200

    App Navigation

--- App.js
+++ App.js
@@ -14,34 +14,57 @@ const Drawer = createDrawerNavigator();
 const MenuIcon = () => <Ionicons name="menu" size={24} color="black" />;
 
 
-function Screen1() {
-  return <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>Screen 1</Text></Layout>;
+function HomeScreen() {
+  return <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>Home Screen, we have to define, whats displayed here.</Text></Layout>;
 }
 
-function Screen2() {
-  return <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>Screen 2</Text></Layout>;
+function MyTicketsScreen() {
+  return <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>List of all open tickets assigned to current agent.</Text></Layout>;
 }
 
-function Screen3() {
-  return <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>Screen 3</Text></Layout>;
+function TeamTicketsScreen() {
+  return <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>List of all open tickets assigned to any team, the current agent is member of, but without the ones displayed in My Tickets.</Text></Layout>;
 }
 
-function MyTab() {
+function ClosedTicketsScreen() {
+  return <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>List of all closed tickets assigned to any team, the current agent is member of.</Text></Layout>;
+}
+
+function UnreadNewsScreen() {
+  return <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>List of all news, which have been not read yet.</Text></Layout>;
+}
+
+function AllNewsScreen() {
+  return <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>List of all news.</Text></Layout>;
+}
+
+
+function Tickets() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Screen1" component={Screen1} />
-      <Tab.Screen name="Screen2" component={Screen2} />
-      <Tab.Screen name="Screen3" component={Screen3} />
+      <Tab.Screen name="My Tickets" component={MyTicketsScreen} />
+      <Tab.Screen name="Team Tickets" component={TeamTicketsScreen} />
+      <Tab.Screen name="Closed Tickets" component={ClosedTicketsScreen} />
     </Tab.Navigator>
   );
 }
 
+function News() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Unread News" component={UnreadNewsScreen} />
+      <Tab.Screen name="All News" component={AllNewsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+
 function MyDrawer() {
   return (
-    <Drawer.Navigator initialRouteName="MyTab">
-      <Drawer.Screen name="MyTab" component={MyTab} />
-      <Drawer.Screen name="MyTab2" component={MyTab} />
-      {/* Add more screens here if needed */}
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Tickets" component={Tickets} />
+      <Drawer.Screen name="News" component={News} />
     </Drawer.Navigator>
   );
 }
@@ -50,7 +73,6 @@ export default function App() {
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
       <NavigationContainer>
-       
         <MyDrawer />
       </NavigationContainer>
     </ApplicationProvider>

885424a03c625540758c4f2d8009ae0586422945
Dave <dev@itsdave.de>
2023-08-14 22:58:43 +0200

    super search gui improvements

--- src/app/(drawer)/superSearch/index.tsx
+++ src/app/(drawer)/superSearch/index.tsx
@@ -1,125 +1,194 @@
 import { DrawerToggleButton } from '@react-navigation/drawer';
-import { Icon, Input, Layout, List, ListItem, Text } from '@ui-kitten/components';
+import {
+  Icon,
+  Input,
+  Layout,
+  List,
+  ListItem,
+  Text,
+  Card,
+} from '@ui-kitten/components';
 import Drawer from 'expo-router/drawer';
 import { useFrappeGetCall } from 'frappe-react-sdk';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, TouchableWithoutFeedback } from 'react-native';
 
 export default function SuperSearchPage(): React.ReactElement {
-    const [searchValue, setSearchValue] = useState('');
-    const [response, setResponse] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [response, setResponse] = useState(null);
+  const [shouldSearch, setShouldSearch] = useState(false);
+  const [hasSearched, setHasSearched] = useState(false);
+  const [isSearching, setIsSearching] = useState(false);
 
-    const frappeGetCall = useFrappeGetCall(
-        'ssc_camp_management.tools.personen_suche',
-        {
-            "value": searchValue,
-            "exact_match": 0,
-            "reservierung_beruecksichtigen": 1,
-            "mitreisende_vorname_beruecksichtigen": 1,
-            "mitreisende_nachname_beruecksichtigen": 1,
-            "past": 0,
-            "present": 1,
-            "future": 0
-        }
-    );
+const formatToGermanDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // +1 because months are 0-indexed
+    const year = date.getFullYear();
 
-    const search = () => {
-        setResponse(frappeGetCall.data);
-        console.log(response);
-        
-    }
+    return `${day}.${month}.${year}`;
+};
 
-//     const renderItem = ({ item, index }): React.ReactElement => {
-// console.log(item);
-// console.log(index);
+  const { data, isLoading } = useFrappeGetCall(
+    shouldSearch ? 'ssc_camp_management.tools.personen_suche' : null,
+    shouldSearch ? {
+      "value": searchValue,
+      "exact_match": 0,
+      "reservierung_beruecksichtigen": 1,
+      "mitreisende_vorname_beruecksichtigen": 1,
+      "mitreisende_nachname_beruecksichtigen": 1,
+      "past": 0,
+      "present": 1,
+      "future": 0
+    } : null
+  );
 
+  if (data && !isLoading) {
+    setResponse(data.message.reser_name);
+    setShouldSearch(false);
+    setIsSearching(false);  // Set isSearching to false when the search results are back
+}
 
-//         const keys = Object.keys(item);
-//         const values = Object.values(item);
-        
-//         return (<ListItem
-//         //   style={styles.item}
-//           title={keys[index]}
-//           description={values[index]}
-//         />
-//         )
-//     };
-
+  const renderCardItem = ({ item }) => {
     return (
-        <Layout style={styles.layoutContainer}>
-            <Drawer.Screen
-                options={{
-                    title: "Super Search",
-                    headerShown: true,
-                    headerLeft: () => <DrawerToggleButton />,
-                }}
-            />
-            <Layout style={styles.formContainer}>
-                <Input
-                    style={styles.inputContainer}
-                    size='medium'
-                    value={searchValue}
-                    label='Search'
-                    placeholder='Place your Text'
-                    accessoryRight={(props) => {
-                        return (
-                            <TouchableWithoutFeedback onPress={() => { search() }}>
-                                <Icon
-                                    {...props}
-                                    name={'search'}
-                                />
-                            </TouchableWithoutFeedback>
-                        )
-                    }}
-                    onChangeText={nextValue => setSearchValue(nextValue)}
-                />
-                {/* <Layout style={styles.formContainer}>
-                    <List
-                        data={response?.message?.reser_name}
-                        renderItem={() => { return renderItem(response) }}
-                    />
-                </Layout> */}
-                <Text>Response: {JSON.stringify(response)}</Text>
-            </Layout>
-        </Layout>
+        <Card style={styles.card}>
+            <Text category="h6">{item.name1} ({item.kundennummer})</Text>
+            <Text>Anreise: {formatToGermanDate(item.anreise)}</Text>
+            <Text>Abreise: {formatToGermanDate(item.abreise)}</Text>
+            <Text>Platz: {item.platz}</Text>
+        </Card>
     );
+};
+
+return (
+    <Layout style={styles.layoutContainer}>
+      <Drawer.Screen
+        options={{
+          title: "Super Search",
+          headerShown: true,
+          headerLeft: () => <DrawerToggleButton />,
+        }}
+      />
+      <Layout style={styles.formContainer}>
+      <Input
+    style={styles.inputContainer}
+    size='medium'
+    value={searchValue}
+    label='Super-Search'
+    placeholder='Suchbegriff eingeben'
+    accessoryRight={(props) => (
+        <TouchableWithoutFeedback onPress={() => {
+            if (searchValue.trim() !== '') {  // Check to ensure the search value isn't empty
+                setShouldSearch(true);
+                setHasSearched(true);
+                setIsSearching(true);  // Set isSearching to true when the search button is pressed
+            }
+        }}>
+            <Icon {...props} name={'search'} />
+        </TouchableWithoutFeedback>
+    )}
+    onChangeText={nextValue => {
+        setSearchValue(nextValue);
+        setResponse(null);
+        setHasSearched(false);  // Reset the hasSearched state here
+    }}
+/>
+
+        
+        {isSearching && (
+            <Layout style={[styles.alertContainer, styles.baseAlert, styles.warningAlert]}>
+                <Text style={styles.warningText}>Suche...</Text>
+            </Layout>
+        )}
+
+  
+        {response === null && !hasSearched && (
+          <Layout style={[styles.alertContainer, styles.baseAlert, styles.infoAlert]}>
+            <Text style={styles.infoText}>Drücke auf die Lupe zum Suchen.</Text>
+          </Layout>
+        )}
+  
+        {response && response.length === 0 && hasSearched && (
+          <Layout style={[styles.alertContainer, styles.baseAlert, styles.errorAlert]}>
+            <Text style={styles.errorText}>Keine Ergebnisse gefunden.</Text>
+          </Layout>
+        )}
+  
+        {response && response.length > 0 && (
+          <List
+            style={styles.listContainer}
+            data={response}
+            renderItem={renderCardItem}
+          />
+        )}
+      </Layout>
+    </Layout>
+  );
+  
 }
 
 const styles = StyleSheet.create({
-    layoutContainer: {
-        flex: 1,
-        alignItems: 'center',
-        padding: 20,
+  layoutContainer: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+  },
+  formContainer: {
+    flex: 1,
+    alignItems: 'flex-start',
+    width: '90%',
+  },
+  listContainer: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: 'white'
+  },
+  inputContainer: {
+    margin: 20
+  },
+  card: {
+    marginVertical: 8,
+    padding: 10,
+    borderRadius: 8,
+    backgroundColor: '#F5F5F5',  // A soft grey background
+    shadowColor: "#000",        // For the shadow
+    shadowOffset: {
+        width: 5,
+        height: 5,
     },
-    formContainer: {
-        flex: 1,
-        alignItems: 'flex-start',
-        width: '90%',
-    },
-    listContainer: {
-        flex: 1,
-        width: '100%',
-    },
-    inputContainer: {
-        margin: 20
-    },
-    button: {
-        alignSelf: 'center'
-    },
-    captionContainer: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    captionIcon: {
-        width: 10,
-        height: 10,
-        marginRight: 5,
-    },
-    captionText: {
-        fontSize: 12,
-        fontWeight: '400',
-        fontFamily: 'opensans-regular',
-        color: '#8F9BB3',
-    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84,
+    elevation: 5,               // This is for Android shadow
+  },
+  alertContainer: {
+    width: '100%',
+    alignItems: 'center', // To center the alert horizontally
+},
+baseAlert: {
+    width: '100%',             // Take full width of the container
+    maxWidth: '100%',          // Don't exceed the container's width
+    borderRadius: 5,
+    padding: 15,
+    marginBottom: 15,
+    alignItems: 'center',      // To center the text inside the alert
+},
+warningAlert: {
+    backgroundColor: '#FFEEBA',
+},
+warningText: {
+    color: '#856404',
+},
+infoAlert: {
+    backgroundColor: '#D1E8FF',
+},
+infoText: {
+    color: '#004085',
+},
+errorAlert: {
+    backgroundColor: '#F8D7DA',
+},
+errorText: {
+    color: '#721C24',
+},
+ 
 });

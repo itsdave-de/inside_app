df8550c23128ff193190485b4e4121d52033c62c
Jordi Albert Ribas González <jordiialb14@gmail.com>
2023-08-19 18:54:26 +0200

    Add floating button to add new tickets
    - Add TouchableOpacity component
    - Add styles

--- src/components/TicketList.tsx
+++ src/components/TicketList.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {View, FlatList, StyleSheet} from 'react-native';
+import { FlatList, StyleSheet, TouchableOpacity } from 'react-native';
 import {Realm} from '@realm/react';
 
 import {Ticket} from '@src/models/Ticket';
 import {TicketItem} from './TicketItem';
-import { Layout } from '@ui-kitten/components';
+import { Icon, Layout, useTheme } from '@ui-kitten/components';
 
 type TicketListProps = {
   tickets: Realm.Results<Ticket>;
@@ -19,8 +19,10 @@ export const TicketList: React.FC<TicketListProps> = ({
   onDeleteTask,
   refreshControl,
 }) => {
+  const theme = useTheme();
+
   return (
-    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+    <Layout style={styles.layoutContainer}>
         <FlatList
           style={styles.listContainer}
           data={tickets}
@@ -34,14 +36,45 @@ export const TicketList: React.FC<TicketListProps> = ({
             />
           )}
         />
+        <TouchableOpacity
+                style={styles.floatingIcon}
+                onPress={() => console.log(`I'm being clicked!`)}
+              >
+                <Icon
+                  name="plus-circle"
+                  style={[styles.icon, { tintColor: theme["color-primary-default"]}]} />
+
+            </TouchableOpacity>
     </Layout>
   );
 };
 
 const styles = StyleSheet.create({
+  layoutContainer: {
+    position: 'relative',
+    height: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    width: '100%' 
+  },
   listContainer: {
     flex: 1,
     width: '100%'
+  },
+  icon: {
+    width: 60,
+    height: 60,
+  },
+  floatingIcon: {
+    position: 'absolute',
+    flex: 1,
+    width: 60,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 25,
+    bottom: 25,
+    elevation: 8,
   },
 });
 
